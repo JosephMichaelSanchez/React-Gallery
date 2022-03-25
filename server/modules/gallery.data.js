39286923b4116180cf20bba0f2a77d790311a1bd
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-03-25 18:52:05 -0400

    Add gallery pics to images and make objects

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }, 
+    { id: 2, path: 'images/boat_sunset.jpeg', description: 'Boat ride during Guys Weekend 2021', likes: 0 },
+    { id: 3, path: 'images/bucks_in_six.jpeg', description: 'Bucks win the title at home in Game 6', likes:0 },
+    { id: 4, path: 'images/dfs_win.jpeg', description: 'Biggest DFS win on NFL opening night 2021', likes: 0 },
+    { id: 5, path: 'images/spring_break_99.jpeg', description: 'What a party animal', likes: 0 },
+    { id: 6, path: 'images/american_outlaw.jpeg', descripton: 'One of Americas Most Wanted', likes: 0 }
 ];
 
 module.exports = galleryItems;
