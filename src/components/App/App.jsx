190a68358adb5113642a4c7c3abb14f92d74ca99
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-03-25 19:49:48 -0400

    Add Axios GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,25 @@ import axios from 'axios';
 
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  const getGalleryList = () => {
+
+    axios.get('/gallery')
+    .then((response) => {
+      console.log('Entire response:', response);
+      console.log('Just the data:', response.data);
+      setGalleryList(response.data);
+    }).catch((error) => {
+      console.log('Error in get:', error);
+    })
+  }
+
+  useEffect(() => {
+    getGallery();
+  }, [])
+
     return (
       <div className="App">
         <header className="App-header">
