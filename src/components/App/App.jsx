6cf75576c8d6809529e34b8a33cc765ffa5c7439
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-03-25 21:16:50 -0400

    Gallery displays

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }
 
   useEffect(() => {
-    getGallery();
+    getGalleryList();
   }, [])
 
     return (
@@ -30,7 +30,12 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/> */}
+        <ul>
+        {galleryList.map( galleryItem => 
+         (<li key={galleryItem.id}><img src={galleryItem.path}/></li>)
+        )}
+      </ul>
       </div>
     );
 }
