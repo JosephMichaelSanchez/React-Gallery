e1ccdde697fef67a7a82ab1168049d7595b56880
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-03-26 23:04:33 -0400

    Save before likeItem function

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -9,7 +9,7 @@ return (
         <div className="flexItem" key={galleryItem.id}>
           <p><img src={galleryItem.path} /></p>
           <p><button>Love It!</button></p>
-          <p>Test text for people liking the picture</p>  
+          <p>{galleryItem.likes} people love this!</p>  
         </div>
     </>
 
